f92e56b6825c35ee8af8b200ecd346bb8300b3d4
Supreet Praharaj <supreet.praharaj.pro@gmail.com>
2024-04-07 21:12:14 +0530

    initialization and adding app.py

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# LlamaBlogCraft-AI
-Blog Generation using Llama 2 llm model
+# Llama Blog Craft
+
+Llama Blog Craft is a Streamlit app that generates blog content using a pre-trained language model.
+
+## Features
+
+- Generate blog content based on user input topic, word count, and target audience.
+- Utilizes a language model to craft coherent and contextually relevant blog posts.
+- Easy-to-use interface powered by Streamlit.
+
+## Requirements
+
+- Python 3.x
+- Streamlit
+- Other dependencies (specified in requirements.txt)
+
+## Installation
+
+1. Clone the repository:
+
+```bash
+git clone https://github.com/your_username/llama-blog-craft.git
+```
+
+2. Install dependencies:
+
+```bash
+cd llama-blog-craft
+pip install -r requirements.txt
+```
+
+3. Download the language model files and place them in a model folder within the
+project directory. The model file llama-2-7b-chat.ggmlv3.q8_0.bin is required 
+for generating blog content. You can obtain this model from:
+[Llama-2-7B-Chat-GGML](https://huggingface.co/TheBloke/Llama-2-7B-Chat-GGML/tree/main).
+
+## Usage
+
+1. Run the streamlit app:
+
+```bash
+streamlit run app.py
+```
+
+2. Enter the blog topic, select the target audience, and specify the word count.
+3. Click on the "Generate" button to generate the blog content.
+
+## Configuration
+
+The language model used for generating blog content is specified in model/llama-2-7b-chat.ggmlv3.q8_0.bin.
+You can adjust parameters such as max_new_tokens and temperature in the get_llama_response function in app.py to fine-tune the output.
+
---
+++ app.py
@@ -0,0 +1,71 @@
+import streamlit as st
+from langchain.prompts import PromptTemplate
+from langchain.llms import CTransformers
+
+
+def get_llama_response(input_text, no_words, blog_style):
+    llm = CTransformers(
+        model="model/llama-2-7b-chat.ggmlv3.q8_0.bin",
+        model_type="llama",
+        config={
+            'max_new_tokens': 256,
+            'temperature': 0.01,
+        }
+    )
+
+    template = """
+        Write a blog for {blog_style} job profile for a topic {input_text}
+        within {no_words} words.
+    """
+
+    prompt = PromptTemplate(
+        input_variables=[
+            "blog_style",
+            "input_text",
+            "no_words",
+        ],
+        template=template
+    )
+
+    response = llm(prompt.format(
+        blog_style=blog_style,
+        input_text=input_text,
+        no_words=no_words,
+    ))
+
+    print(response)
+
+    return response
+
+
+st.set_page_config(
+    page_title='Llama Blog Craft',
+    page_icon='🤖',
+    layout='centered',
+    initial_sidebar_state='collapsed',
+)
+
+st.header('Llama Blog Craft - Generate Blogs 🤖')
+
+input_text = st.text_input("Enter the blog topic")
+
+col1, col2 = st.columns([5, 5])
+
+with col1:
+    no_words = st.text_input("No of words")
+
+with col2:
+    blog_style = st.selectbox(
+        "Write the blog for",
+        (
+            "Researchers",
+            "Data Scientist",
+            "Common People",
+        ),
+        index=0
+    )
+
+submit = st.button("Generate")
+
+if submit:
+    st.write(get_llama_response(input_text, no_words, blog_style))
